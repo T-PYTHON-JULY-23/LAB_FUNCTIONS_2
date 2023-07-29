92300f129921f124afcad722217f17eaeb90d5a0
sara <sara@hotmail.com>
2023-07-30 00:05:05 +0300

    lab_functions2

--- lab_functions2.py
+++ lab_functions2.py
@@ -1,9 +1,12 @@
 def prime(number1, number2 ):
+    
+    print(f"primes between {number1} and {number2} are:")
     for i in range(number1, number2 ):
                 for j in range(2 , i):  
                     if (i % j) == 0:
                         break
                 else:
+
                     print(i)
 
 prime(25,50)
