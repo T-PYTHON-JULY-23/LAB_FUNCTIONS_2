920fca70801ec16d5ec3acbae66297dd6d59c419
AbdulazizAlzahrani <abdulaziz.alzahrani.14@outlook.com>
2023-07-29 12:48:06 +0300

    add comments

--- bonus.py
+++ bonus.py
@@ -3,6 +3,16 @@ def fix_str(sentance:str) -> str:
     if type(sentance) == str:
         new_sent = ""
         for chr in sentance:
+            '''
+            every letter or sympol have a Unicode, 
+            Unicode is simply a number to represante a letter or sympol, 
+            here i want to compare the later in it's original case and the lower case
+            if they are not equale then the letter is in the upper case i have to add 
+            space befor the letter and change the case of the letter to be lower case 
+            if not the i just need append it normaly.
+            for example :
+            ord("W") != ord("w") will return True.
+            '''
             if ord(chr) != ord(chr.lower()):
                 new_sent += f" {chr.lower()}"
             else:
