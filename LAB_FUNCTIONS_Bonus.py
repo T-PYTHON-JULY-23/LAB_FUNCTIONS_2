25e6926898395f416b62af55191620b29c78bfa2
nada <nada_abdullah_8@hotmail.com>
2023-07-31 10:11:03 +0300

    lab-2-func

---
+++ LAB_FUNCTIONS_Bonus.py
@@ -0,0 +1,20 @@
+
+
+
+
+def modifiedString (phrase:str)-> str:
+    """ check the type and replace it with a small letter,separates the word then return modified string"""
+
+    for i in range ("phrase"):
+      
+      if phrase==phrase.__str__ and phrase == phrase.upper:
+       phrase = phrase + " "
+     
+    
+    
+
+    return phrase.lower
+
+
+
+modifiedString("HelloWord")
